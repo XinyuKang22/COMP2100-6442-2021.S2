6ae40f1cd3282764741bf5ec5e95d00affa34a69
Xinyu Kang <u6120911@anu.edu.au>
2021-10-07 02:36:39 +1100

    1

--- My_Practice/COMP6466/QuickSelect.java
+++ My_Practice/COMP6466/QuickSelect.java
@@ -29,41 +29,39 @@ public class QuickSelect {
         System.out.println(weights);
         System.out.println(xs);
         System.out.println(ys);
+
+        System.out.println("x median:");
+        float[] xMed = quickSelect(weights, xs, ys,0, n-1, 0, 0);
+        System.out.println(xMed[0]);
+        System.out.println(xMed[1]);
+        System.out.println("y median:");
+        float[] yMed = quickSelect(weights, ys, xs,0, n-1, 0, 0);
+        System.out.println(yMed[0]);
+        System.out.println(yMed[1]);
+
+        System.out.println("");
+        System.out.println(weights);
+        System.out.println(xs);
+        System.out.println(ys);
+
     }
 
-    public static float[] partition(ArrayList<Float> weightList, ArrayList<Integer> xList, ArrayList<Integer> yList, int p, int r){
+    public static float[] partition(ArrayList<Float> weightList, ArrayList<Integer> xList,ArrayList<Integer> yList, int p, int r){
         int x = xList.get(r);
-        int y = yList.get(r);
         int i = p - 1;
         float left = 0;
         float right = 0;
         float[] toReturn = new float[3];
 
         for (int j = p; j < r; j ++){
-            if (compareTo(xList.get(j),yList.get(j),x,y) <= 0){  // is ok to = 0???
+            if (xList.get(j) <= x){  // is ok to = 0???
                 left = left + weightList.get(j);
                 i = i + 1;
-                int xb = xList.get(j);
-                int yb = yList.get(j);
-                float wb = weightList.get(j);
-                xList.set(j,xList.get(i));
-                yList.set(j, yList.get(i));
-                weightList.set(j, weightList.get(i));
-                xList.set(i, xb);
-                yList.set(i, yb);
-                weightList.set(i, wb);
+                swap(weightList, xList, yList, j, i);
             }else {
                 right = r + weightList.get(j);
             }
-            int xb = xList.get(i + 1);
-            int yb = yList.get(i + 1);
-            float wb = weightList.get(i + 1);
-            xList.set(i + 1,xList.get(r));
-            yList.set(i + 1, yList.get(r));
-            weightList.set(i + 1, weightList.get(r));
-            xList.set(r, xb);
-            yList.set(r, yb);
-            weightList.set(r, wb);
+            swap(weightList, xList, yList, i+1,r);
         }
         toReturn[0] = i + 1;
         toReturn[1] = left;
@@ -71,54 +69,46 @@ public class QuickSelect {
         return toReturn;
     }
 
-    public static float[] pivot(ArrayList<Float> weightList, ArrayList<Integer> xList, ArrayList<Integer> yList, int p, int r){
+    public static float[] pivot(ArrayList<Float> weightList, ArrayList<Integer> xList,ArrayList<Integer> yList, int p, int r){
         Random random = new Random();
         int i = p + random.nextInt(r - p + 1);
-        int xb = xList.get(r);
-        int yb = yList.get(r);
-        float wb = weightList.get(r);
-        xList.set(r,xList.get(i));
-        yList.set(r, yList.get(i));
-        weightList.set(r, weightList.get(i));
-        xList.set(i, xb);
-        yList.set(i, yb);
-        weightList.set(i, wb);
-        return partition(weightList, xList, yList, p, r);
+        swap(weightList, xList, yList, r, i);
+        return partition(weightList, xList,yList, p, r);
     }
 
-    public static float[] quickSelect(ArrayList<Float> weightList, ArrayList<Integer> xList, ArrayList<Integer> yList, int p, int r, float leftSum, float rightSum){
-        float[] toReturn = new float[3];
+    public static float[] quickSelect(ArrayList<Float> weightList, ArrayList<Integer> xList, ArrayList<Integer> yList,int p, int r, float leftSum, float rightSum){
+        float[] toReturn = new float[2];
         if (p < r){
-            float[] pivotResult = pivot(weightList, xList, yList, p, r);
-            int q = Integer.parseInt(String.valueOf(pivotResult[0]));
+            float[] pivotResult = pivot(weightList, xList,yList, p, r);
+            int q = Math.round(pivotResult[0]);
             float left = pivotResult[1];
             float right = pivotResult[2];
             if (leftSum + left < rightSum + right){
-                return quickSelect(weightList, xList, yList, q+1, r, leftSum+left, rightSum);
+                return quickSelect(weightList, xList,yList, q+1, r, leftSum+left, rightSum);
             }else if (leftSum + left > rightSum + right){
-                return quickSelect(weightList, xList, yList, p, q-1, leftSum, rightSum+right);
+                return quickSelect(weightList, xList,yList, p, q-1, leftSum, rightSum+right);
             }else {
                 toReturn[0] = weightList.get(q);
                 toReturn[1] = xList.get(q);
-                toReturn[2] = yList.get(q);
                 return toReturn;
             }
         }
-        return null;
+        toReturn[0] = weightList.get(p);
+        toReturn[1] = xList.get(p);
+        return toReturn;
     }
 
+    public static void swap(ArrayList<Float> weightList, ArrayList<Integer> xList, ArrayList<Integer> yList,int i1, int i2){
+        int xb = xList.get(i1);
+        int yb = yList.get(i1);
+        float wb = weightList.get(i1);
 
+        xList.set(i1,xList.get(i2));
+        yList.set(i1,yList.get(i2));
+        weightList.set(i1, weightList.get(i2));
 
-
-
-    public static int compareTo(int x1, int y1, int x2, int y2){
-        if (x1 != x2){
-            return x1 - x2;
-        }else {
-            return y1 - y2;
-        }
+        xList.set(i2, xb);
+        yList.set(i2, yb);
+        weightList.set(i2, wb);
     }
-
-
-
 }
