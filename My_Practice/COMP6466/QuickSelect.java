ad3ea76830cfd4d8daca2bedabff92a90010099c
Xinyu Kang <u6120911@anu.edu.au>
2021-10-07 04:58:08 +1100

    1

--- My_Practice/COMP6466/QuickSelect.java
+++ My_Practice/COMP6466/QuickSelect.java
@@ -16,56 +16,72 @@ public class QuickSelect {
         ArrayList<Integer> ys = new ArrayList<>();
 
         try(BufferedReader br = new BufferedReader(new FileReader(fileName))) {
-            n = Integer.parseInt(br.readLine());
+            n = Integer.parseInt(br.readLine().split("\\s+")[0]);
             for(String line; (line = br.readLine()) != null; ) {
                 String[] content = line.split("\\s+");
                 weights.add(Float.parseFloat(content[0]));
                 xs.add(Integer.parseInt(content[1]));
                 ys.add(Integer.parseInt(content[2]));
             }
-            // line is not visible here.
         }
         System.out.println(n);
         System.out.println(weights);
         System.out.println(xs);
         System.out.println(ys);
 
-        System.out.println("x median:");
         float[] xMed = quickSelect(weights, xs, ys,0, n-1, 0, 0);
+        float[] yMed = quickSelect(weights, ys, xs,0, n-1, 0, 0);
+
+        System.out.println("");
+        float sumx = 0;
+        float sumy = 0;
+        for (int i = 0; i < n; i ++){
+            int x = xs.get(i);
+            int y = ys.get(i);
+            if (x <= xMed[1]){
+                sumx = sumx + weights.get(i);
+            }
+            if (y <= yMed[1]){
+                sumy = sumy + weights.get(i);
+            }
+        }
+        System.out.println("x median:");
         System.out.println(xMed[0]);
         System.out.println(xMed[1]);
         System.out.println("y median:");
-        float[] yMed = quickSelect(weights, ys, xs,0, n-1, 0, 0);
         System.out.println(yMed[0]);
         System.out.println(yMed[1]);
-
-        System.out.println("");
-        System.out.println(weights);
-        System.out.println(xs);
-        System.out.println(ys);
-
+        System.out.println("validation:");
+        System.out.println(sumx);
+        System.out.println(sumy);
     }
 
-    public static float[] partition(ArrayList<Float> weightList, ArrayList<Integer> xList,ArrayList<Integer> yList, int p, int r){
-        int x = xList.get(r);
-        int i = p - 1;
-        float left = 0;
-        float right = 0;
-        float[] toReturn = new float[3];
-
-        for (int j = p; j < r; j ++){
-            if (xList.get(j) <= x){  // is ok to = 0???
-                left = left + weightList.get(j);
-                i = i + 1;
-                swap(weightList, xList, yList, j, i);
+    public static float[] quickSelect(ArrayList<Float> weightList, ArrayList<Integer> xList, ArrayList<Integer> yList,int p, int r, float leftSum, float rightSum){
+        float[] toReturn = new float[2];
+        if (p < r){
+            float[] pivotResult = pivot(weightList, xList,yList, p, r);
+            int q = Math.round(pivotResult[0]);
+            float left = pivotResult[1];
+            float right = pivotResult[2];
+            float mid = weightList.get(q);
+            if (leftSum + left == rightSum + right || leftSum + left + mid== rightSum + right){
+                toReturn[0] = weightList.get(q);
+                toReturn[1] = xList.get(q);
+                System.out.println("quickselect return: "+toReturn[0]+", "+toReturn[1]);
+                return toReturn;
+            }else if (leftSum + left < rightSum + right && !(leftSum + left +mid >= rightSum + right)){
+                return quickSelect(weightList, xList,yList, q+1, r, leftSum+left+mid, rightSum);
+            }else if (leftSum + left > rightSum + right && !(rightSum + right + mid >= leftSum + left)){
+                return quickSelect(weightList, xList,yList, p, q-1, leftSum, rightSum+right+mid);
             }else {
-                right = r + weightList.get(j);
+                toReturn[0] = weightList.get(q);
+                toReturn[1] = xList.get(q);
+                System.out.println("quickselect return: "+toReturn[0]+", "+toReturn[1]);
+                return toReturn;
             }
-            swap(weightList, xList, yList, i+1,r);
         }
-        toReturn[0] = i + 1;
-        toReturn[1] = left;
-        toReturn[2] = right;
+        toReturn[0] = weightList.get(r);
+        toReturn[1] = xList.get(r);
         return toReturn;
     }
 
@@ -76,25 +92,26 @@ public class QuickSelect {
         return partition(weightList, xList,yList, p, r);
     }
 
-    public static float[] quickSelect(ArrayList<Float> weightList, ArrayList<Integer> xList, ArrayList<Integer> yList,int p, int r, float leftSum, float rightSum){
-        float[] toReturn = new float[2];
-        if (p < r){
-            float[] pivotResult = pivot(weightList, xList,yList, p, r);
-            int q = Math.round(pivotResult[0]);
-            float left = pivotResult[1];
-            float right = pivotResult[2];
-            if (leftSum + left < rightSum + right){
-                return quickSelect(weightList, xList,yList, q+1, r, leftSum+left, rightSum);
-            }else if (leftSum + left > rightSum + right){
-                return quickSelect(weightList, xList,yList, p, q-1, leftSum, rightSum+right);
+    public static float[] partition(ArrayList<Float> weightList, ArrayList<Integer> xList,ArrayList<Integer> yList, int p, int r){
+        int x = xList.get(r);
+        int i = p - 1;
+        float left = 0;
+        float right = 0;
+        float[] toReturn = new float[3];
+
+        for (int j = p; j < r; j ++){
+            if (xList.get(j) <= x){
+                left = left + weightList.get(j);
+                i = i + 1;
+                swap(weightList, xList, yList, j, i);
             }else {
-                toReturn[0] = weightList.get(q);
-                toReturn[1] = xList.get(q);
-                return toReturn;
+                right = right + weightList.get(j);
             }
         }
-        toReturn[0] = weightList.get(p);
-        toReturn[1] = xList.get(p);
+        swap(weightList, xList, yList, i+1,r);
+        toReturn[0] = i + 1;
+        toReturn[1] = left;
+        toReturn[2] = right;
         return toReturn;
     }
 
