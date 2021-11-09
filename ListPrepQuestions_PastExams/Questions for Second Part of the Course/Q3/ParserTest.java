aa16443f84773c65d3736d04f9e1920be7bcbbe3
Bernardo Pereira Nunes <u1064702@anu.edu.au>
2021-11-09 23:42:10 +0000

    Update ParserTest.java: fixed test cases

--- ListPrepQuestions_PastExams/Questions for Second Part of the Course/Q3/ParserTest.java
+++ ListPrepQuestions_PastExams/Questions for Second Part of the Course/Q3/ParserTest.java
@@ -25,12 +25,12 @@ public class ParserTest {
 
     @Test
     public void testMoreNumbers() {
-        assertFalse(new Parser(new Tokeniser("446325")).parseExp());
+        assertFalse(new Parser(new Tokeniser("44")).parseExp());
         assertTrue(new Parser(new Tokeniser("425")).parseExp());
         assertTrue(new Parser(new Tokeniser("1425")).parseExp());
         assertTrue(new Parser(new Tokeniser("146325")).parseExp());
 
-        assertFalse(new Parser(new Tokeniser("353618")).parseExp());
+        assertFalse(new Parser(new Tokeniser("88")).parseExp());
         assertTrue(new Parser(new Tokeniser("136")).parseExp());
         assertTrue(new Parser(new Tokeniser("1436")).parseExp());
         assertTrue(new Parser(new Tokeniser("146336")).parseExp());
