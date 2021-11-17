47d7212106be4e09e966e011fc207f720a8a37da
Xinyu Kang <u6120911@anu.edu.au>
2021-11-17 12:02:16 +1100

    1

---
+++ A_FinalExam/q2/Parser.java
@@ -0,0 +1,23 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class Parser {
+
+	private final Tokeniser tokeniser;
+
+	public Parser(Tokeniser tokeniser) {
+		this.tokeniser = tokeniser;
+	}
+
+	public List<Command> parseCmds() {
+
+		List<Command> commands = new LinkedList<>();
+
+		// TODO
+		// ########## YOUR CODE STARTS HERE ##########
+
+		// ########## YOUR CODE ENDS HERE ##########
+
+		return commands;
+	}
+}
